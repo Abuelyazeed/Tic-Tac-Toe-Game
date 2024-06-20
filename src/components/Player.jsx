842c7334fce4d4164cfde2df2410806cca4d18df
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-20 10:16:37 +0300

    updated player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? (
+          <input type="text" required />
+        ) : (
+          <span className="player-name">{name}</span>
+        )}
+
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
