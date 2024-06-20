be4326f567184d630e6843eac8200f81324900e0
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-20 10:51:30 +0300

    added functionality to the player component

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,29 @@
 import React, { useState } from "react";
 
-function Player({ name, symbol }) {
+function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleEditClick() {
     setIsEditing((editing) => !editing);
   }
+
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
   return (
     <li>
       <span className="player">
         {isEditing ? (
-          <input type="text" required />
+          <input
+            type="text"
+            required
+            value={playerName}
+            onChange={handleChange}
+          />
         ) : (
-          <span className="player-name">{name}</span>
+          <span className="player-name">{playerName}</span>
         )}
 
         <span className="player-symbol">{symbol}</span>
