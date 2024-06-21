35cd1b29c38767c7c61b4f8b1d874aa8f6d64d13
Abuelyazeed <abuelyazeed175@gmail.com>
2024-06-21 11:41:19 +0300

    updated the Log component

--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,7 +1,11 @@
 function Log({ turns }) {
   return (
     <ol id="log">
-      <li></li>
+      {turns.map((turn) => (
+        <li key={`${turn.square.row}${turn.square.col}`}>
+          {turn.player} selected {turn.square.row},{turn.square.col}
+        </li>
+      ))}
     </ol>
   );
 }
